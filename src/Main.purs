e905455a63749c5ae759cb5aef27a8844e272b79
Rémi Vion <vion.remi@gmail.com>
2016-12-16 18:12:16 +0100

    ensure main call P1 and P2 so psc-ide watch everything

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,11 @@
 module Main where
 
 import Prelude
+import P1 as P1
+import P2 as P2
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import P1.Solution as P1
 
+main :: Eff _ Unit
 main = do
   P1.solve
-  -- P1.solve
---
+  P2.main
