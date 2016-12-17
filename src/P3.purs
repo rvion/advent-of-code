fafab8d54c201f7e5779378fb4fe37ae21578c8d
Rémi Vion <vion.remi@gmail.com>
2016-12-17 06:47:43 +0100

    P3.2

--- src/P3.purs
+++ src/P3.purs
@@ -2,34 +2,43 @@ module P3 where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Data.Array (filter, foldl, index, length, scanl, sort)
+import Control.Monad.Eff.Console (logShow)
+import Data.Array (concat, cons, drop, filter, length, take, uncons)
 import Data.Foldable (and)
 import Data.Int (fromString)
-import Data.JSDate (isValid)
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Ord (abs)
-import Data.String (Pattern(..), charAt, fromCharArray, split, toCharArray)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.String (Pattern(Pattern), split)
 import Partial.Unsafe (unsafeCrashWith)
 import Util (getFile)
 
+type Triangle = Array Int
+
 test :: Eff _ Unit
 test = do
-  file <- getFile "src/p3.txt"
-  logShow (solve1 file)
+  input <- getFile "src/p3.txt"
+  logShow $ "917 == " <> show (solve input extract1)
+  logShow $ "??? == " <> show (solve input extract2)
+  logShow $ [[1,2,3],[4,5,6],[7,8,9]] # chunks 3
+    # map transpose
+    # concat
 
--- solve1 :: String -> Array (Array String)
--- solve1 :: String -> Array (Array Int)
-solve1 :: String -> Int
-solve1 str = filter isValid triangles # length
+extract1 :: String -> Array Triangle
+extract1 str = lines # map toTriangle
   where
     lines = split (Pattern "\n") str
-    triangles = lines # map toTriangle
     toTriangle = split (Pattern " ")
       >>> filter (_ /= "")
       >>> map (fromString >>> unsafeFromMaybe)
-      -- >>> sort
 
+extract2 :: String -> Array Triangle
+extract2 str = extract1 str
+  # chunks 3
+  # map transpose
+  # concat
+
+solve :: String -> (String -> Array Triangle) -> Int
+solve str extract = filter isValid (extract str) # length
+  where
     isValid :: Array Int -> Boolean
     isValid = case _ of
       [a,b,c] -> and
@@ -38,11 +47,20 @@ solve1 str = filter isValid triangles # length
         , b+c > a]
       _ -> false
 
-answer1 :: Int
-answer1 = 917
-
 unsafeFromMaybe :: forall a. Maybe a -> a
 unsafeFromMaybe = case _ of
   Nothing -> unsafeCrashWith "maybe was not a just"
   Just a -> a
--- unsafeFromMaybe2 = fromMaybe (unsafeCrashWith "maybe was not a just 2")
+
+chunks :: forall a. Int -> Array a -> Array (Array a)
+chunks _ [] = []
+chunks n xs = cons (take n xs) (chunks n $ drop n xs)
+
+transpose :: forall a. Array (Array a) -> Array (Array a)
+transpose m = case uncons m of
+  Just {head} -> case uncons head of
+    Just val -> cons
+      (map (take 1) m # concat)
+      (transpose (m # map (drop 1)))
+    _ -> []
+  _ -> []
