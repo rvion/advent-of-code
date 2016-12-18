cdd99e836f116b28c21b237099e720dc0b91921a
Rémi Vion <vion.remi@gmail.com>
2016-12-18 13:29:55 +0100

    P4.2

--- src/P4.purs
+++ src/P4.purs
@@ -5,10 +5,13 @@ import Data.Array as A
 import Data.NonEmpty as NE
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (logShow)
-import Data.Array (filter, group, many, sort, sortBy, take)
+import Data.Array (filter, find, group, many, sort, sortBy, take)
+import Data.Char (fromCharCode, toCharCode)
 import Data.Either (Either(..))
 import Data.Foldable (sum)
-import Data.String (Pattern(Pattern), Replacement(Replacement), fromCharArray, replaceAll, split, toCharArray)
+import Data.Maybe (Maybe(..))
+import Data.String (Pattern(Pattern), Replacement(Replacement), contains, fromCharArray, replaceAll, split, toCharArray)
+import Data.Tuple (Tuple(..), fst, snd)
 import ParseUtils (lowerCaseLetter, parseInt)
 import Partial.Unsafe (unsafeCrashWith)
 import Text.Parsing.Parser (ParseError, runParser)
@@ -20,6 +23,7 @@ test = do
   input <- getFile "src/p4.txt"
   let validRooms = solve input
   logShow $ "158835 == " <> show (solve1 validRooms)
+  logShow $ "   993 == " <> show (solve2 validRooms)
 
 solve :: String -> Array Room
 solve str = str
@@ -32,6 +36,31 @@ solve1 :: Array Room -> Int
 solve1 rooms = rooms
   # map (_.uid)
   # sum
+
+solve2 :: Array Room -> Int
+solve2 rooms = case mbRoom of
+  Nothing -> unsafeCrashWith "no solution found"
+  Just sol -> fst sol
+  where
+    mbRoom = rooms
+      # map (\{uid,name} -> Tuple uid $ name
+        # toCharArray
+        # map (shift uid)
+        # fromCharArray
+        )
+      # find (snd >>> contains (Pattern "northpoleobjectstorage"))
+
+shift :: Int -> Char -> Char
+shift x c = c
+  # toCharCode
+  # (_ - aCode)
+  # (_ + x)
+  # (_ `mod` 26)
+  # add aCode
+  # fromCharCode
+  where
+   aCode = toCharCode 'a'
+
 
 isValid :: Room -> Boolean
 isValid {realChecksum, givenChecksum} =
