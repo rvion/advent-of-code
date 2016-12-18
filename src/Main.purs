3d7c71b4d24b7ef499e1913c86ad0f2633c4ed84
Rémi Vion <vion.remi@gmail.com>
2016-12-18 17:06:32 +0100

    update Main

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,9 @@ import Prelude
 import P1 as P1
 import P2 as P2
 import P3 as P3
+import P4 as P4
+import P5 as P5
+
 import Control.Monad.Eff (Eff)
 
 main :: Eff _ Unit
@@ -11,3 +14,5 @@ main = do
   P1.solve
   P2.test
   P3.test
+  P4.test
+  P5.test
